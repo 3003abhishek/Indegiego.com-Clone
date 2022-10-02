68855e88b06db421bfc8cc6b9a16ec111334427d
Manishg <manscans16@gmail.com>
2022-10-02 17:12:37 +0530

    final

---
+++ component/footer.js
@@ -0,0 +1,84 @@
+function footer(){
+    return ` <div>
+    <div id="footer">
+        <div id="main">
+           <div>
+            <p>EXPLORE</p>
+            <div class="div">
+                <a href="">Wat We Do</a>
+                <a href="">Funding</a>
+            </div>
+          </div>
+            <div >
+                <p>ABOUT</p>
+                <div class="div">
+                    <a href="">About Us</a>
+                    <a href="">Blog</a>
+                    <a href="">Trust & Saftey</a>
+                    <a href="">Help & Support</a>
+                    <a href="">Press</a>
+                    <a href="">Careers</a>
+                    <a href="">Contect</a>
+                </div>
+            </div>
+            <div >
+                <p>ENTERPRENEURS</p>
+                <div class="div">
+                    <a href="">How It Works</a>
+                    <a href="">Indiagogo vs. Kickstarter</a>
+                    <a href="">Education Center</a>
+                    <a href="">Experts Directory</a>
+                    <a href="">Fees</a>
+                    <a href="">Enterprise</a>
+                    <a href="">China</a>
+                </div>
+            </div>
+           
+        </div>
+        <div id="main2">
+        <div id="sign">
+            <img src="https://img.freepik.com/premium-vector/pink-envelope-with-document-concept-receive-message-email-subscribe-vector_605517-38.jpg?w=740" alt="">
+            <h3>Find it first on Indiegogo</h3>
+            <p>Discover new and clever products in the Indiegogo newsletter</p>
+            <input type="text" placeholder="Your email address">
+          <div class="check">
+            <input type="checkbox">
+            <div id="terms">
+            <span>I agree to the</span>
+            <a href="">Terms of Use</a>
+            <span>and have read and understand the</span>
+            <a href="">Privacy Policy</a>
+         </div>
+          </div>
+          <button>SIGN ME UP</button>
+
+        </div>
+    </div>
+
+    </div>
+    <div id="bottemdiv">
+    <div id="bottem">
+       <input type="text" placeholder="Indian Rupee(INR)">
+      <a href="https://www.facebook.com/Indiegogo"><img src="https://cdn-icons-png.flaticon.com/128/3128/3128208.png" alt=""></a> 
+      <a href="https://twitter.com/indiegogo"><img src="https://cdn-icons-png.flaticon.com/128/733/733635.png" alt=""></a> 
+      <a href="https://www.youtube.com/indiegogo"><img src="https://cdn-icons-png.flaticon.com/128/1384/1384028.png" alt=""></a>
+       <a href="https://www.instagram.com/indiegogo/"><img src="https://cdn-icons-png.flaticon.com/128/4494/4494468.png" alt=""></a> 
+       <a href="https://www.linkedin.com/company/indiegogo/"><img src="https://cdn-icons-png.flaticon.com/128/3536/3536569.png" alt=""></a>
+       <a href="https://open.spotify.com/show/7bEdWFbpv3rKHotpDzcCV4?si=6a5cee9902214695&nd=1"><img src="https://cdn-icons-png.flaticon.com/128/3116/3116491.png" alt=""></a>
+    </div>
+    <hr>
+    <div class="end">
+    <a href="">Terms of Use</a>
+    <a href="">Privacy Policy</a>
+    <a href="">Cookie Policy</a>
+    <a href="">Do Not Sell Mu Personal In formation</a>
+    <a href="">Accessibilty</a>
+    <span>© 2022 Indiegogo, Inc. All Rights Reserved</span>
+   </div>
+</div> 
+</div> 
+</div> 
+   `
+}
+
+export {footer}
